321b2aa6f212f1844b75ca124feeeee842fc7993
Steamcollect <eliot.garnier.080805@gmail.com>
2025-01-07 14:09:36 +0100

    Feat : basic enemy system

---
+++ Assets/Weapon_Sword.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Sword : WeaponTemplate
+{
+    [Header("Settings")]
+    [SerializeField] float attackDistance = 2;
+
+    //[Header("References")]
+
+    //[Space(10)]
+    // RSO
+    // RSF
+    // RSP
+
+    //[Header("Input")]
+    //[Header("Output")]
+    public override void Attack(Vector3 attackPosition, Vector3 attackDirection)
+    {
+        transform.forward = attackDirection;
+
+        RaycastHit[] hits = Physics.RaycastAll(attackPosition, attackDirection, attackDistance);
+
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.transform.TryGetComponent(out IHealth health))
+                {
+                    health.TakeDamage(damage);
+                }
+            }            
+        }
+    }
+
+    public override bool CanAttack()
+    {
+        return true;
+    }
+
+    public override void Reload()
+    {
+        // Do nothing
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * attackDistance);
+    }
+}
