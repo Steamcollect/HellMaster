5bac933a9161ba09b27f8bb0720815074b5e49e8
Steamcollect <eliot.garnier.080805@gmail.com>
2025-01-07 16:58:45 +0100

    Feat : swap weapon with scroll

--- Assets/App/Scripts/Entity/Combat/PlayerCombat.cs
+++ Assets/App/Scripts/Entity/Combat/PlayerCombat.cs
@@ -1,10 +1,14 @@
 using System.Collections;
-using UnityEditor.PackageManager;
+using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerCombat : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] WeaponTemplate weapon;
+    [SerializeField] Transform weaponsContent;
+
+    [SerializeField] List<WeaponTemplate> weapons = new();
+    [SerializeField] int currentWeaponIndex = 0;
     [SerializeField] Transform cam;
 
     //[Space(10)]
@@ -17,19 +21,39 @@ public class PlayerCombat : MonoBehaviour
 
     private void Start()
     {
-        weapon.Reload();
+        weapons[currentWeaponIndex].Reload();
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && weapon.isSemiAuto || Input.GetKeyDown(KeyCode.Mouse0))
+        int newWeaponIndex = (currentWeaponIndex + (int)Input.mouseScrollDelta.y) % weapons.Count;
+        if(newWeaponIndex < 0) newWeaponIndex = weapons.Count - 1;
+
+        if(newWeaponIndex != currentWeaponIndex)
         {
-            weapon.OnAttack(cam.position, cam.forward);
+            weapons[currentWeaponIndex].gameObject.SetActive(false);
+            weapons[newWeaponIndex].gameObject.SetActive(true);
+            currentWeaponIndex = newWeaponIndex;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && weapons[currentWeaponIndex].isSemiAuto || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            weapons[0].OnAttack(cam.position, cam.forward);
         }
 
         if (Input.GetKey(KeyCode.R))
         {
-            weapon.Reload();
+            weapons[currentWeaponIndex].Reload();
         }
+    }
+
+    void AddWeapon(WeaponTemplate newWeaponPrefab)
+    {
+        weapons.Add(Instantiate(newWeaponPrefab, weaponsContent));
+
+        int newWeaponIndex = weapons.Count - 1;
+        weapons[currentWeaponIndex].gameObject.SetActive(false);
+        weapons[newWeaponIndex].gameObject.SetActive(true);
+        currentWeaponIndex = newWeaponIndex;
     }
 }
---
+++ Assets/App/Scripts/Management/AchievmentManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AchievmentManager : MonoBehaviour
+{
+    //[Header("Settings")]
+
+    //[Header("References")]
+
+    //[Space(10)]
+    // RSO
+    // RSF
+    // RSP
+
+    //[Header("Input")]
+    //[Header("Output")]
+}
