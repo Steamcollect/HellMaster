3c2bd166aaf7f2368c2bbb76c1f5a79360baae1d
Steamcollect <eliot.garnier.080805@gmail.com>
2025-01-24 15:59:27 +0100

    Feat : global volum system

---
+++ Assets/App/Scripts/VisualEffects/PostProcess.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcess : MonoBehaviour
+{
+    //[Header("Settings")]
+
+    [Header("References")]
+    [SerializeField] Volume globalVolum;
+
+    Vignette vignette;
+    Sequence vignetteSequence;
+    float vignetteIntensityValue;
+
+    //[Space(10)]
+    // RSO
+    // RSF
+    // RSP
+
+    //[Header("Input")]
+
+    [Header("Output")]
+    [SerializeField] RSE_OnShoot rseOnShoot;
+
+    private void OnEnable()
+    {
+        rseOnShoot.action += BumpVignetteValue;
+    }
+    private void OnDisable()
+    {
+        rseOnShoot.action -= BumpVignetteValue;
+    }
+
+    private void Awake()
+    {
+        if (globalVolum.profile.TryGet(out Vignette _vignette)) vignette = _vignette;
+    }
+
+    private void Start()
+    {
+        vignetteIntensityValue = vignette.intensity.value;
+    }
+
+    void BumpVignetteValue()
+    {
+        DOTween.Kill(vignetteSequence);
+
+        vignetteSequence = DOTween.Sequence(
+            DOTween.To(() => vignetteIntensityValue, x => vignetteIntensityValue = x, 
+                1/*End Value*/ , 
+                1/*Time*/)
+                    .OnUpdate(() =>
+                    {
+                        vignette.intensity.value = vignetteIntensityValue;
+                    })
+                    .OnComplete(() =>
+                    {
+
+                        DOTween.To(() => vignetteIntensityValue, x => vignetteIntensityValue = x,
+                            1/*End Value*/ ,
+                            1/*Time*/)
+                                .OnUpdate(() =>
+                                {
+                                    vignette.intensity.value = vignetteIntensityValue;
+                                });
+                    })
+            );
+    }
+}
